956a3673f519cfd8a46bad4cef10ba54b5315d32
matheusdasmerces <matheusdasmerces@postnl.nl>
2025-01-14 11:32:18 +0100

    Initial commit

---
+++ test/cdk-fine-grained-tests.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkFineGrainedTests from '../lib/cdk-fine-grained-tests-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-fine-grained-tests-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkFineGrainedTests.CdkFineGrainedTestsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
